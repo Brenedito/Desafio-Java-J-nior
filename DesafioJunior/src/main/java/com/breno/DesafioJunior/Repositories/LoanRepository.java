83b26039374799799ae3a2d6fc1ba94550362c52
BrenoBritoDev <breno.brito@outlook.com>
2025-09-09 14:36:21 -0300

    Criando método para contagem de livros alugados pelos usuários

--- DesafioJunior/src/main/java/com/breno/DesafioJunior/Repositories/LoanRepository.java
+++ DesafioJunior/src/main/java/com/breno/DesafioJunior/Repositories/LoanRepository.java
@@ -3,8 +3,13 @@ package com.breno.DesafioJunior.Repositories;
 
 import com.breno.DesafioJunior.Models.LoanModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoanRepository extends JpaRepository<LoanModel, Long> {
+
+    @Query("SELECT COUNT(books) FROM LoanModel books WHERE books.user.user_id = :user_id")
+    Integer countByUserId(@Param("user_id") Long userid);
 }
